e02082a0d3349ad52d00a0943946807340c7444c
Macarena061199 <102336411+Macarena061199@users.noreply.github.com>
2022-04-02 17:24:29 -0300

    Listo

--- Program.cs
+++ Program.cs
@@ -3,3 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
+    ConsoleKeyInfo key;
+    Console.WriteLine("Por favor presione cualquier tecla. ");
+    Console.WriteLine("Si desea salir presione Ctrl+F.");
+
+//Me guardo F
+    ConsoleKey f = ConsoleKey.F;
+//Me guardo Ctl
+    ConsoleModifiers ctl = ConsoleModifiers.Control;
+
+
+do
+{
+    
+
+    key = Console.ReadKey();
+    Console.WriteLine("" + System.Environment.NewLine +
+        "Usted presiono " + key.KeyChar);
+
+    if ((key.Modifiers & ConsoleModifiers.Control) != 0)
+    {
+        Console.Write("CTL+");
+    }
+
+    Console.WriteLine(key.Key.ToString());
+
+} while (key.Modifiers != ctl || key.Key != f);
